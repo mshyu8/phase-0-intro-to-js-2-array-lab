13302faba699462a84c3f5795ea78f00ccece4f8
Michael Shyu <michaelshyu11@gmail.com>
2023-11-05 20:04:34 -0500

    Finshed required functions

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Write your solution here!
+let cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+    cats.push(name);
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+}
+
+function appendCat(name) {
+    let copyCat = cats.slice();
+    copyCat.push(name);
+    
+    return copyCat;
+}
+
+function prependCat(name) {
+    let copyCat = [name, ...cats];
+    return copyCat;
+}
+
+function removeLastCat() {
+    let copyCat = cats.slice(0, cats.length - 1);
+    
+    return copyCat;
+}
+
+function removeFirstCat() {
+    let copyCat = cats.slice(1);
+
+    return copyCat;
+}
